a9cda64e21f0bafb9f67ed28d3a0f903f7087c42
huangj <xyhuangjia@gmail.com>
2025-07-07 13:43:52 +0800

    feat：支持图片圆状

--- mvvmhabit/src/main/java/me/goldze/mvvmhabit/binding/viewadapter/image/ViewAdapter.java
+++ mvvmhabit/src/main/java/me/goldze/mvvmhabit/binding/viewadapter/image/ViewAdapter.java
@@ -4,6 +4,7 @@ import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 
 import androidx.databinding.BindingAdapter;
@@ -12,14 +13,22 @@ import androidx.databinding.BindingAdapter;
  * Created by goldze on 2017/6/18.
  */
 public final class ViewAdapter {
-    @BindingAdapter(value = {"url", "placeholderRes"}, requireAll = false)
-    public static void setImageUri(ImageView imageView, String url, int placeholderRes) {
+    @BindingAdapter(value = {"url", "placeholderRes", "round"}, requireAll = false)
+    public static void setImageUri(ImageView imageView, String url, int placeholderRes,boolean round) {
         if (!TextUtils.isEmpty(url)) {
             //使用Glide框架加载图片
-            Glide.with(imageView.getContext())
-                    .load(url)
-                    .apply(new RequestOptions().placeholder(placeholderRes))
-                    .into(imageView);
+            if (round) {
+                Glide.with(imageView.getContext())
+                        .load(url)
+                        .apply(new RequestOptions().placeholder(placeholderRes))
+                        .apply(RequestOptions.bitmapTransform(new CircleCrop()))
+                        .into(imageView);
+            } else {
+                Glide.with(imageView.getContext())
+                        .load(url)
+                        .apply(new RequestOptions().placeholder(placeholderRes))
+                        .into(imageView);
+            }
         }
     }
 }
